f33cdc6bf5335b0ecabc7d7f850cf527d50d0535
Nordine Bittich <nordine@bittich.be>
2022-03-06 19:31:21 +0100

    fix multi comments

--- parser/src/ntriple.rs
+++ parser/src/ntriple.rs
@@ -125,8 +125,13 @@ fn parse_list_triples<'a>(s: &'a str) -> IResult<&'a str, Vec<Triple<'a>>> {
     ))(s)
 }
 
-fn skip_comment<'a>(s: &'a str) -> IResult<&'a str, Option<&'a str>> {
-    opt(preceded(char('#'), take_until("\n")))(s)
+fn skip_comment<'a>(s: &'a str) -> IResult<&'a str, Vec<&'a str>> {
+    many0(
+        terminated(
+        preceded(char('#'), take_until("\n")),
+        multispace0
+    )
+    )(s)
 }
 
 #[cfg(test)]
@@ -146,6 +151,12 @@ mod tests {
          <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
          <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
          <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
+         #  the entire line is commented <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
+         #  the entire line is commented <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
+         #  the entire line is commented <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
+
+
+         
             #  the entire line is commented <http://bittich.be/some/url/123>    <http://example.org/firstName><http://n.com/nordine>  .
             _:alice <http://xmlns.com/foaf/0.1/knows> _:bob .
             _:bob <http://xmlns.com/foaf/0.1/knows> _:alice .
@@ -174,6 +185,6 @@ mod tests {
          <http://en.wikipedia.org/wiki/Helium> <http://example.org/elements/specificGravity> "1.663E-4"^^<http://www.w3.org/2001/XMLSchema#double> .     # xsd:double
          "#;
         let (remaining, triples) = parse_list_triples(triples).unwrap();
-        println!("{:?}", remaining);
+        println!("{:?}", triples);
     }
 }
